3e1db7a7fb7401eb3a26533c3ed9739f702d8d64
Vennela Dupati <venneladupati@gmail.com>
2023-09-18 23:38:20 -0500

    what is going on bruh

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 from urllib.request import urlopen, Request
 from bs4 import BeautifulSoup
-#import warnings
-#from bs4 import GuessedAtParserWarning
-#warnings.filterwarnings('ignore', category=GuessedAtParserWarning)
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import pandas as pd
+import matplotlib.pyplot as plt
+import warnings
+import dateutil.parser as parser
+import datetime as dt
+from bs4 import GuessedAtParserWarning
+warnings.filterwarnings('ignore', category=GuessedAtParserWarning)
+
 
 finvizUrl = 'https://finviz.com/quote.ashx?t='
-tickers = ['AMZN', 'FB', 'AMD']
+tickers = ['AMZN', 'AAPL', 'TSLA']
 
 newsTables={}
 for ticker in tickers:
@@ -17,16 +23,45 @@ for ticker in tickers:
     html = BeautifulSoup(response,'html')
     newsTable = html.find(id='news-table')
     newsTables[ticker] = newsTable
-    break
 
-amznData = newsTables['AMZN']
-amznRows = amznData.findAll('tr')
+parsedData= []
 
-for index, row in enumerate(amznRows):
-        if row.a is not None:
+for ticker, newsTable in newsTables.items():
+
+    for row in newsTable.findAll('tr'):
+
+        if row.a != None:
             title = row.a.text
-            timestamp = row.td.text
-            print(timestamp + " " + title)
 
+            dateData = row.td.text.strip().split(" ")
+
+
+            if len(dateData) == 1:
+                time = dateData[0]
+            else:
+                date = dateData[0]
+                time = dateData[1]
+
+            parsedData.append([ticker,date,time,title])
+        
+    df = pd.DataFrame(parsedData, columns = ['ticker', 'date', 'time', 'title'])
+    vader = SentimentIntensityAnalyzer()
+    
+    f = lambda title: vader.polarity_scores(title)['compound']
+    df['compound'] = df['title'].apply(f)
+    df['date'] = pd.to_datetime(df.date).dt.date
+
+
+    print(df)
+
+    '''plt.figure(figsize=(10,8))
+
+    meanDf = df.groupby(['ticker', 'date']).mean(numeric_only=True)
+    meanDf = meanDf.unstack()
+    
+    meanDf = meanDf.xs('compound', axis="columns").transpose()
+    meanDf.plot(kind='bar')
+    plt.show()'''
+    
 
 
